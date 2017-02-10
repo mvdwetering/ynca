713d0637c17e0ac1f97e05129d7799f5de9e7374
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2017-02-10 11:08:39 +0000

    Added thread for handling sending and keepalive

--- ynca_test.py
+++ ynca_test.py
@@ -4,15 +4,7 @@ import time
 import traceback
 import sys
 import threading
-
-
-class RepeatingTimer(threading.Timer):
-    def run(self):
-        while not self.finished.is_set():
-            self.finished.wait(self.interval)
-            self.function(*self.args, **self.kwargs)
-
-        self.finished.set()
+import queue
 
 
 class YncaProtocol(serial.threaded.LineReader):
@@ -23,32 +15,26 @@ class YncaProtocol(serial.threaded.LineReader):
     # YNCA spec says standby timeout is 40 seconds, so use 30 seconds to be on the safe side
     KEEP_ALIVE_INTERVAL = 5
 
-    def __init__(self):
-        super(YncaProtocol, self).__init__()
-
-        def _send_keepaliveXYZ():
-            sys.stdout.write("Keep the dream alive! ")
-            self.get('SYS','MODELNAME')
-
-        self._keepalive_timer = RepeatingTimer(self.KEEP_ALIVE_INTERVAL, _send_keepaliveXYZ())
-       
-
 
     def connection_made(self, transport):
         super(YncaProtocol, self).connection_made(transport)
         sys.stdout.write('port opened\n')
 
-        self._timestamp_last_command = 0
-
-        self._keepalive_timer.start()
+        self._send_queue = queue.Queue()
+        self._send_thread = threading.Thread(target=self._send_handler)
+        self._send_thread.start()
 
         # When the device is in low power mode the first command is to wake up 
-        # So send a dummy keep-alive on connect (which also starts the keepalive sequence)
-        #self._send_keepalive()
+        # So send a dummy keep-alive on connect
+        self._send_keepalive()
 
     def connection_lost(self, exc):
-        #self._keepalive_timer.cancel()
-        #self._keepalive_timer = None
+        # There seems to be no way to clear the queue so just read all and  add the _EXIT command
+        try:
+            while self._send_queue.get(False):
+                pass
+        except queue.Empty:
+            self._send_queue.put("_EXIT")
 
         if exc:
             sys.stdout.write(exc)
@@ -58,23 +44,26 @@ class YncaProtocol(serial.threaded.LineReader):
     def handle_line(self, line):
         sys.stdout.write(repr(line))
 
-    def _send_keepalive2(self):
-        self.get('SYS','MODELNAME')
+    def _send_keepalive(self):
+        self.get('SYS','MODELNAME') # This message is suggested by YNCA spec for keep-alive
 
-        # To avoid random message being eaten because device goes to sleep, keep it alive
-        #if not self._keepalive_timer:
-        #self._keepalive_timer = threading.Timer(self.KEEP_ALIVE_INTERVAL, self._send_keepalive())
-        #self._keepalive_timer.start()
-        
+    def _send_handler(self):
+        exit = False
+        while not exit:
+            try:
+                message = self._send_queue.get(True, self.KEEP_ALIVE_INTERVAL)
+            
+                if message == "_EXIT":
+                    exit = True
+                else:
+                   self.write_line(message)
+                   time.sleep(self.COMMAND_INTERVAL) # Maintain required commandspacing
+            except queue.Empty:
+                # To avoid random message being eaten because device goes to sleep, keep it alive
+                self._send_keepalive()
 
     def put(self, subunit, funcname, parameter):
-        # Maintain required commandspacing (not nice that it blocks, but works for now FIXME)
-        time_since_last_command = time.monotonic() - self._timestamp_last_command
-        if time_since_last_command < self.COMMAND_INTERVAL:
-            time.sleep(self.COMMAND_INTERVAL - time_since_last_command)
-
-        self.write_line('@{subunit}:{funcname}={parameter}'.format(subunit=subunit, funcname=funcname, parameter=parameter))
-        self._timestamp_last_command = time.monotonic()
+        self._send_queue.put('@{subunit}:{funcname}={parameter}'.format(subunit=subunit, funcname=funcname, parameter=parameter))
 
     def get(self, subunit, funcname):
         self.put(subunit, funcname, '?')
@@ -87,7 +76,7 @@ with serial.threaded.ReaderThread(ser, YncaProtocol) as protocol:
     remaining = 10
     while remaining >= 0:
       print("Remaining: {}".format(remaining))
-      protocol.get('SYS','VERSION')
+      #protocol.get('SYS','VERSION')
       time.sleep(1)
       remaining = remaining - 1
 
