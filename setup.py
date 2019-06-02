50e589f767487022a426a18d3a033a16268c6bf8
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2019-06-02 20:39:53 +0200

    Bump version and promote to stable

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.0',
+    version='1.0.0',
 
     description='Automation control library for Yamaha receivers that support the YNCA protocol.',
     long_description=long_description,
@@ -44,7 +44,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
