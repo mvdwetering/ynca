09b7ca3c2ea4d2264917c167459aa413951d2248
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-02-27 19:11:06 +0100

    Add Ynca class to manage connection, receiver and zones

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ import sys
 import time
 import logging
 
-import ynca
+from ynca import Ynca, Mute
 
-""" Yeah, not really a test, more ofa script to see things are not breaking horribly. """
+""" Yeah, not really a test, more of a script to see things are not breaking horribly. """
 
 update_number = 1
 
 if __name__ == "__main__":
+
     def updated():
         global update_number
         print("Update {}".format(update_number))
@@ -23,40 +24,48 @@ if __name__ == "__main__":
     logger = logging.getLogger()
 
     console_handler = logging.StreamHandler()
-    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    console_handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    )
     logger.addHandler(console_handler)
     logger.setLevel(logging.INFO)
-    #logging.getLogger("ynca.connection").setLevel(logging.DEBUG)
+    logging.getLogger("ynca.connection").setLevel(logging.DEBUG)
+    logging.getLogger("ynca.receiver").setLevel(logging.DEBUG)
+    logging.getLogger("ynca.zone").setLevel(logging.DEBUG)
 
     port = "/dev/ttyUSB0"
     if len(sys.argv) > 1:
         port = sys.argv[1]
 
-    receiver = ynca.YncaReceiver(port, updated)
-    receiver.zones["MAIN"].on_update_callback = updated_zone1
-    receiver.zones["ZONE2"].on_update_callback = updated_zone2
+    ynca = Ynca.create_from_serial_url(port)
 
-    print(receiver)
+    ynca.receiver.register_update_callback(updated)
+    ynca.zones["MAIN"].register_update_callback(updated_zone1)
+    ynca.zones["ZONE2"].register_update_callback(updated_zone2)
+
+    print(ynca.receiver)
 
     print("Zones:")
-    for zone in receiver.zones:
-        print("--- {} ---".format(zone))
-        print(receiver.zones[zone])
+    for id, zone in ynca.zones.items():
+        print("--- {} ---".format(id))
+        print(zone)
 
     print("Inputs:")
-    print(receiver.inputs)
+    print(ynca.receiver.inputs)
 
-    receiver.zones["MAIN"].volume = -50
-    receiver.zones["MAIN"].volume = -50.2
-    receiver.zones["MAIN"].volume = -50.4
-    receiver.zones["MAIN"].volume = -50.5
-    receiver.zones["MAIN"].volume = -50.7
-    receiver.zones["MAIN"].volume = -50.8
-    receiver.zones["MAIN"].volume = -51.0
-    receiver.zones["MAIN"].volume = -35
+    ynca.zones["MAIN"].volume = -50
+    ynca.zones["MAIN"].volume = -50.2
+    ynca.zones["MAIN"].volume = -50.4
+    ynca.zones["MAIN"].volume = -50.5
+    ynca.zones["MAIN"].volume = -50.7
+    ynca.zones["MAIN"].volume = -50.8
+    ynca.zones["MAIN"].volume = -51.0
+    ynca.zones["MAIN"].volume = -35
 
-    receiver.zones["ZONE2"].mute = ynca.Mute.off
-    receiver.zones["ZONE2"].mute = ynca.Mute.on
+    ynca.zones["ZONE2"].on = True
+    ynca.zones["ZONE2"].mute = Mute.off
+    ynca.zones["ZONE2"].mute = Mute.on
+    ynca.zones["ZONE2"].on = False
 
     remaining = 2
     while remaining >= 0:
@@ -64,3 +73,4 @@ if __name__ == "__main__":
         time.sleep(1)
         remaining -= 1
 
+    ynca.close()
--- ynca/__init__.py
+++ ynca/__init__.py
@@ -1,8 +1,48 @@
 import logging
+from typing import Dict
 
-from .connection import ynca_console
+from .connection import YncaConnection, ynca_console
 from .constants import DSP_SOUND_PROGRAMS, Mute
 from .receiver import YncaReceiver
 from .zone import YncaZone
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+class Ynca:
+    def __init__(
+        self,
+        connection: YncaConnection,
+        receiver: YncaReceiver,
+        zones: Dict[str, YncaZone],
+    ):
+        """This constructor is intended to be called from the factorymethod create_from_serial_url"""
+        self._connection = connection
+        self.receiver = receiver
+        self.zones = zones
+
+    @staticmethod
+    def create_from_serial_url(serial_url: str):
+        """
+        Creates a Ynca instance with an initialized connection, receiver and zones.
+        This call takes several seconds.
+        """
+        connection = YncaConnection(serial_url)
+        connection.connect()
+
+        receiver = YncaReceiver(connection)
+        receiver.initialize()
+
+        zones: Dict[str, YncaZone] = {}
+        for zone_id in receiver.zones:
+            zone = YncaZone(zone_id, connection, receiver.inputs)
+            zone.initialize()
+            zones[zone_id] = zone
+
+        return Ynca(connection, receiver, zones)
+
+    def close(self):
+        self.receiver.close()
+        for zone in self.zones.values():
+            zone.close()
+        self._connection.close()
