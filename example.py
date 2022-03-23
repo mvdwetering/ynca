c46dee1da488b8f8c9e958603c9cd6800703e09c
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-03-23 22:17:45 +0100

    Improve example

--- example.py
+++ example.py
@@ -7,17 +7,13 @@ import logging
 
 from ynca import Receiver, Mute, ZONES, Subunit
 
-update_number = 1
-
 if __name__ == "__main__":
 
     def updated():
-        global update_number
-        print("Update {}".format(update_number))
-        update_number += 1
+        print("- Update sys")
 
-    def updated_zone1():
-        print("- Update zone1")
+    def updated_main():
+        print("- Update main")
 
     def updated_zone2():
         print("- Update zone2")
@@ -29,25 +25,28 @@ if __name__ == "__main__":
         logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     )
     logger.addHandler(console_handler)
-    logger.setLevel(logging.INFO)
-    logging.getLogger("ynca.connection").setLevel(logging.DEBUG)
-    logging.getLogger("ynca.receiver").setLevel(logging.DEBUG)
-    logging.getLogger("ynca.system").setLevel(logging.DEBUG)
-    logging.getLogger("ynca.zone").setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+    # logging.getLogger("ynca.connection").setLevel(logging.DEBUG)
+    # logging.getLogger("ynca.receiver").setLevel(logging.DEBUG)
+    # logging.getLogger("ynca.subunit").setLevel(logging.DEBUG)
+    # logging.getLogger("ynca.system").setLevel(logging.DEBUG)
+    # logging.getLogger("ynca.zone").setLevel(logging.DEBUG)
 
     port = "/dev/ttyUSB0"
     if len(sys.argv) > 1:
         port = sys.argv[1]
 
     receiver = Receiver(port)
+    print("Initialize start")
     receiver.initialize()
+    print("Initialize done")
 
     sys = receiver.subunits[Subunit.SYS]
     main = receiver.subunits[Subunit.MAIN]
     zone2 = receiver.subunits[Subunit.ZONE2]
 
     sys.register_update_callback(updated)
-    main.register_update_callback(updated_zone1)
+    main.register_update_callback(updated_main)
     zone2.register_update_callback(updated_zone2)
 
     print(sys)
@@ -57,7 +56,9 @@ if __name__ == "__main__":
         try:
             zone = receiver.subunits[subunit_id]
             print("--- {} ---".format(zone.id))
-            print(zone)
+            print(f"{zone.name=}")
+            print(f"{zone.volume=}")
+            print(f"{zone.input=}")
         except KeyError:
             pass
 
@@ -67,24 +68,21 @@ if __name__ == "__main__":
     print("Subunits:")
     print(receiver.subunits)
 
+    main.on = True
+    current_volume = main.volume
     main.volume = -50
-    main.volume = -50.2
-    main.volume = -50.4
     main.volume = -50.5
-    main.volume = -50.7
-    main.volume = -50.8
-    main.volume = -51.0
-    main.volume = -35
+    main.volume_up()
+    main.volume = current_volume
 
     zone2.on = True
     zone2.mute = Mute.off
     zone2.mute = Mute.on
     zone2.on = False
 
-    remaining = 2
-    while remaining >= 0:
-        print("Remaining: {}".format(remaining))
-        time.sleep(1)
-        remaining -= 1
+    print(
+        "Wait a bit to see updates coming in as it takes the receiver a while to get it all processed"
+    )
+    time.sleep(2)
 
     receiver.close()
