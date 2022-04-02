1ad1c0997a3a1df45516929bda7aa4e5e2406564
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-04-02 17:59:08 +0200

    Bump version to 3.0.1 and fix description_content_type

--- setup.py
+++ setup.py
@@ -23,9 +23,10 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="3.0.0",
+    version="3.0.1",
     description="Automation control library for Yamaha receivers that support the YNCA protocol.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     # The project's main homepage.
     url="https://github.com/mvdwetering/ynca",
     # Author details
