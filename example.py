4227f874f6f3c18dfda352c09af71b90714e61f1
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-06-17 14:45:25 +0200

    Update example to match README, handle new exception

--- example.py
+++ example.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-""" A bit of a messy example/manual test script. """
+""" Example/manual test script. """
 
 import sys
 import time
 import logging
 
-from ynca import Ynca, Mute, Subunit, get_all_zone_inputs
+from ynca import (
+    Ynca,
+    Mute,
+    Subunit,
+    YncaException,
+    get_all_zone_inputs,
+)
 
 ZONE_SUBUNIT_IDS = [Subunit.MAIN, Subunit.ZONE2, Subunit.ZONE3, Subunit.ZONE4]
 
@@ -19,18 +25,25 @@ if __name__ == "__main__":
         logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     )
     logger.addHandler(console_handler)
-    logger.setLevel(logging.DEBUG)
+    # logger.setLevel(logging.DEBUG)
 
     port = "/dev/ttyUSB0"
     if len(sys.argv) > 1:
         port = sys.argv[1]
 
-    receiver = Ynca(port)
+    ynca_receiver = Ynca(port)
 
     print("Initialize start")
     print("This takes a while (approximately 10 seconds on a 2 zone receiver)")
-    receiver.initialize()
-    print("Initialize done\n")
+    try:
+        ynca_receiver.initialize()
+        print("Initialize done\n")
+    except YncaException as e:
+        print("\n-- Exception during initialization")
+        print(f"-- Exception class: {type(e).__name__}")
+        print(f"-- Description: {e.__doc__}")
+        print(f"-- Details: {e}")
+        exit(1)
 
     def updated_sys():
         print("- Update sys")
@@ -38,24 +51,28 @@ if __name__ == "__main__":
     def updated_main():
         print("- Update main")
 
-    receiver.SYS.register_update_callback(updated_sys)
-    receiver.MAIN.register_update_callback(updated_main)
+    ynca_receiver.SYS.register_update_callback(updated_sys)
+    ynca_receiver.MAIN.register_update_callback(updated_main)
 
     print("Zones:")
     for subunit_id in ZONE_SUBUNIT_IDS:
-        if zone := getattr(receiver, subunit_id, None):
+        if zone := getattr(ynca_receiver, subunit_id, None):
             print("  --- {} ---".format(zone.id))
             print(f"  {zone.name=}")
             print(f"  {zone.volume=}")
             print(f"  {zone.input=}")
 
     print("Inputs:")
-    for id, name in get_all_zone_inputs(receiver).items():
+    for id, name in get_all_zone_inputs(ynca_receiver).items():
         print(f"  {id}: {name}")
 
-    main = receiver.MAIN
+    # Set loglevel to debug so you can see the commands
+    # sent because of the statements below
+    logger.setLevel(logging.DEBUG)
+
+    main = ynca_receiver.MAIN
     main.pwr = True
-    current_volume = main.volume
+    current_volume = main.volume  # Save so we can restore it
     main.volume = -50
     main.volume = -50.5
     main.volume_up()
@@ -66,7 +83,6 @@ if __name__ == "__main__":
     print(
         "Wait a bit to see updates coming in as it takes the receiver a while to get it all processed."
     )
-    print("This is more insightful when enabling debug logging.")
     time.sleep(2)
 
-    receiver.close()
+    ynca_receiver.close()
