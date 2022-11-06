6036db21487763bd8b5e693089265a993007bb68
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-11-06 19:46:59 +0100

    Update BT subunit

--- ynca/bt.py
+++ ynca/bt.py
@@ -1,16 +1,14 @@
 from __future__ import annotations
 
-import logging
 
 from .constants import Subunit
-from .function_mixins import (
-    PlaybackFunctionMixin,
-)
+from .function_mixins import PlaybackFunctionMixin
 from .subunit import SubunitBase
 
 
 class Bt(
-    PlaybackFunctionMixin,
     SubunitBase,
 ):
     id = Subunit.BT
+
+    playback = PlaybackFunctionMixin.playback
