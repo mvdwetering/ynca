005f7160f1b76c71720e3dab8d4c3cbc18fdc665
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-11-13 15:01:29 +0100

    Update package description

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version="3.11.0",
-    description="Automation control library for Yamaha receivers that support the YNCA protocol.",
+    description="Package to control Yamaha receivers that support the YNCA protocol.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     # The project's main homepage.
