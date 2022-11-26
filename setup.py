97a45ac4a2847b203376f84f0b5c5559c0debc73
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2022-11-26 16:27:48 +0100

    Bump version to 4.0.0a2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="4.0.0a1",
+    version="4.0.0a2",
     description="Package to control Yamaha receivers that support the YNCA protocol.",
     long_description=long_description,
     long_description_content_type="text/markdown",
