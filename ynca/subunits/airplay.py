72d95c3c485b6e03c36168559ef9d9dace178ed7
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2023-02-14 20:43:24 +0100

    Add Album, Artist, Song, PlaybackInfo to AirPlay

--- ynca/subunits/airplay.py
+++ ynca/subunits/airplay.py
@@ -1,12 +1,22 @@
 from __future__ import annotations
 
 from ..constants import Subunit
-from . import PlaybackFunction
 from ..subunit import SubunitBase
+from . import (
+    AlbumFunction,
+    ArtistFunction,
+    PlaybackFunction,
+    PlaybackInfoFunction,
+    SongFunction,
+)
 
 
 class Airplay(
+    AlbumFunction,
+    ArtistFunction,
     PlaybackFunction,
+    PlaybackInfoFunction,
+    SongFunction,
     SubunitBase,
 ):
     id = Subunit.AIRPLAY
