94ddf21f1b617563c0f846ef750ebceef3eebfab
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2025-03-29 18:28:25 +0100

    Comment all public functions in connection class

--- ynca/connection.py
+++ ynca/connection.py
@@ -19,6 +19,16 @@ logger = logging.getLogger(__name__)
 class YncaConnection:
     @classmethod
     def create_from_serial_url(cls, serial_url: str) -> YncaConnection:
+        """Create a YncaConnection instance.
+
+        serial_url:
+            Can be a devicename (e.g. /dev/ttyUSB0 or COM3),
+            but also any of supported url handlers by pyserial
+            https://pyserial.readthedocs.io/en/latest/url_handlers.html
+            This allows to setup IP connections with socket://ip:50000
+            or select a specific usb-2-serial with hwgrep:// which is
+            useful when the links to ttyUSB# change randomly.
+        """
         return cls(serial_url)
 
     def __init__(self, serial_url: str) -> None:
@@ -50,6 +60,7 @@ class YncaConnection:
             [YncaProtocolStatus, str | None, str | None, str | None], None
         ],
     ) -> None:
+        """Register a callback to be called when a message is received."""
         self._message_callbacks.add(callback)
 
     def unregister_message_callback(
@@ -58,6 +69,7 @@ class YncaConnection:
             [YncaProtocolStatus, str | None, str | None, str | None], None
         ],
     ) -> None:
+        """Unregister a previously registered callback."""
         self._message_callbacks.discard(callback)
 
     def _call_registered_message_callbacks(
@@ -81,6 +93,14 @@ class YncaConnection:
         communication_log_size: int = 0,
     ) -> None:
         """Connect to the receiver.
+
+        disconnect_callback:
+            Will be called when the connection is lost. It will _not_ be called when `close()` is called explicitly.
+
+        communication_log_size:
+            Amount of communication items to log. Useful for debugging.
+            Get the logged items with the `get_communication_log_items` method
+        """
         try:
             with self._disconnect_callback_lock:
                 self._disconnect_callback = disconnect_callback
@@ -103,6 +123,7 @@ class YncaConnection:
             raise YncaConnectionFailed from e
 
     def close(self) -> None:
+        """Close the connection."""
         # Disconnect callback is for unexpected disconnects
         # Don't need it to be called on planned `close()`
         with self._disconnect_callback_lock:
@@ -112,23 +133,28 @@ class YncaConnection:
             self._readerthread.close()
 
     def raw(self, raw_data: str) -> None:
+        """Send raw data to the receiver."""
         if self._protocol:
             self._protocol.raw(raw_data)
 
     def put(self, subunit: str, funcname: str, parameter: str) -> None:
+        """Send a PUT request to set a value of a function on a subunit of the receiver."""
         if self._protocol:
             self._protocol.put(subunit, funcname, parameter)
 
     def get(self, subunit: str, funcname: str) -> None:
+        """Send a GET request to get a value of a function on a subunit of the receiver. Note that only a request is sent, no response is awaited."""
         if self._protocol:
             self._protocol.get(subunit, funcname)
 
     @property
     def connected(self) -> bool:
+        """Indicates if connection is connected or not."""
         return self._protocol.connected if self._protocol else False
 
     @property
     def num_commands_sent(self) -> int:
+        """Get the amount of commands sent."""
         return self._protocol.num_commands_sent if self._protocol else 0
 
     def get_communication_log_items(self) -> list[str]:
